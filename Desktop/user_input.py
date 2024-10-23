b6517cbbd0f17799bfdf25b5effc7178b3c07e77
jerry <nivahjerry@gmail.com>
2024-10-24 01:08:21 +0300

    pushing the user_input.py file

---
+++ Desktop/user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = str(input("How old are you?"))
+location = input("Where do you live?")
+greetings = "Hello " + name + " You are " + age + " years old and live in " + location
+print(greetings)
